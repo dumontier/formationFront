8b3522c719f0e360b912cccab4595121512cddd4
dumontier <40385152+dumontier@users.noreply.github.com>
2018-09-01 18:46:55 +0200

    date age 01/09

--- 01-js/assets/js/07-exercice1.js
+++ 01-js/assets/js/07-exercice1.js
@@ -23,33 +23,9 @@
 // }
 // document.write(administration());
 
-// // -- 1. Initialisation des Variables
-// var objetDate = new Date();
-// var anneeActuelle = objetDate.getFullYear();
-
-// // -- 2. Création d'une fonction
-// function Indentity() {
-
-// // -- 3. Je demande à l'utilisateur son Prénom
-//     var prenom = prompt("Bonjour! quel es ton nom", "<Saisir ton prenom>");
-//     console.log(prenom);
-//     console.log(typeof prenom);
-
-// // -- 4. Je lui demande son age
-//     var age = prompt("Bonjour" + prenom + "Quel age as tu ","<Saisir ton age>");
-//     console.log(age);
-//     console.log(typeof age);
-
-// // -- 5. Déduire l'année de naissance et l'afficher dans un alerte
-// var anneeDeNaissance = anneeActuelle - age;
-// alert("AMAZIN! So you were born in" + anneeDeNaissance);
-
-// // -- 6. Récapitulatif dans la page
-// document.write("bonjour" + prenom + "tu es" + age + "Tu es nait en ");
-// }
-
-// // -- 7. Executer la fonction
-// Indentity();
+/*************************************************
+                    CORRECTION
+*************************************************/
 
 // -- 1. Initialisation des Variables
 var objetDate = new Date();
@@ -70,7 +46,7 @@ console.log(typeof age);
 
 // -- 5. Déduire l'année de naissance et l'afficher dans un alerte
 var anneeDeNaissance = anneeActuelle - age;
-alert("Pas possible ! tu es nait en " + anneeDeNaissance);
+alert("Pas possible! tu es nait en " + anneeDeNaissance);
 
 // -- 6. Récapitulatif dans la page
 document.write("bonjour" + " " + prenom + " " + "tu as " + age + " ans tu es, donc nait en " + anneeDeNaissance);
